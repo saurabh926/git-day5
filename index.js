64d47704a33e942be41f935a2b66dc91679cf0fb
saurabh926 <saurabhrawat9826@gmail.com>
2024-06-25 23:10:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-elfjbwoiwep9hjfsfewf
+// elfjbwoiwep9hjfsfewf
+
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Assuming you will add some CSS for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Brand</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
