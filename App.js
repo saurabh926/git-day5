aa9d62ef444ad6cec505fcd407619510e50be0c9
saurabh926 <saurabhrawat9826@gmail.com>
2024-06-25 23:15:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Contact Us</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
