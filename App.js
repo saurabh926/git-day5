8449bdabe66694638ef0c33a89e749301232ec6c
saurabh926 <saurabhrawat9826@gmail.com>
2024-06-29 17:57:41 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 // src/App.js
 import React from 'react';
 import Form from './components/Form';
+import RegistrationForm from './components/RegistrationForm';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Contact Us</h1>
       <Form />
+      <h1>Register</h1>
+      <RegistrationForm />
     </div>
   );
 };
