f515d735080376dc68935dc940c95071b69d7076
saurabh926 <saurabhrawat9826@gmail.com>
2024-07-09 12:50:06 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,14 @@
+const index=()=>{
+
+    return(
+        <>
+        <input type="text"  />  <h1>Cybrom Connect</h1>
+        Codemithra is a leading education and placement platform that helps students achieve certification and get placements in large MNCs.
+
+We help students improve their aptitude skills, communication and interpersonal abilities thus making them industry-ready.
+
+      
+        </>
+    )
+}
+export default index;
