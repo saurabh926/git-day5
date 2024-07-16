ca13870244f6cccfc8d6aa106a535b1481407c95
saurabh926 <saurabhrawat9826@gmail.com>
2024-07-16 16:01:35 +0530

    first commit

--- Navbar.js
+++ Navbar.js
@@ -1,11 +1,22 @@
-
-
-const Navbar=()=>{
-    return(
-        <>
-         <h1>hello ritu </h1>
-        </>
-    )
+import React from 'react'
+// import img1 from "../assets/dbvmk0tsk0o0hd59fi64z3own.png";
+const Landing = () => {
+  return (
+   <>
+  <section >
+    <div className="container  ">
+      <div className="row">
+      <h1 className='let1'>Let the right people know youre open to work</h1>
+      <h3 className='let2'>With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.</h3>
+    </div>
+    <div ><img className='img2' src="https://png.pngtree.com/png-vector/20240309/ourmid/pngtree-we-are-open-hanging-signboard-now-icon-banner-sign-illustration-vector-png-image_11905814.png" alt="" /></div>
+    </div>
+    <div className='container1'>
+    <h1>Find the right job or internship for you</h1>  
+    </div>
+  </section>
+   </>
+  )
 }
 
-export default Navbar;
+export default Landing
